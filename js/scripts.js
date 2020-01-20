69c2427eda0d1b2e7acd7bb299b6355eefd2eb85
Olabee87 <53159103+Olabee87@users.noreply.github.com>
2020-01-20 14:22:06 +0200

    Changes + airline example

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,38 @@ window.dataLayer.forEach(function(event) {
     "</p></div>";
   document.getElementById("event-list").appendChild(newEvent);
 });
+
+/*  var helsinkiAirportDeparture = [
+
+{
+    airlines: 'norwegian',
+    destination: 'oslo',
+    departure: '11:00'
+},
+{
+    airlines: 'finnair',
+    destination: 'helsinki',
+    departure: '12:00'
+},
+{
+    airlines: 'british airways',
+    destination: 'london',
+    departure: '10:00'
+}
+
+];
+
+helsinkiAirportDeparture.forEach(function(flight){
+
+    var newFlight = document.createElement('li');
+    newFlight.innerHTML.HTML = '<div class="info"><h2 class ="titile">' 
+            + flight.airlines 
+            + '</h2><h2 class="desc">'
+            + flight.destination
+            + '</h2></div>';
+
+            document.getElementById('departure-list').appendChild(newFlight);
+});
+
+
+ */
